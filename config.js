8c98c215fbb4c0c0721082f92922e9458652fa5f
MltrCyber <52839806+MltrCyber@users.noreply.github.com>
2021-07-20 08:12:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['6281515860089','6281334177340'] // Put your number here
+global.owner = ['62882015535456','6282273128721'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -8,7 +8,7 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'd90a9e986e18778b'
+  'https://api.xteam.xyz': 'dfcd8a99ad75270b'
 }
 
 // Sticker WM
